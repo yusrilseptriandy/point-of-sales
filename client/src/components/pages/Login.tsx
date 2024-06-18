74883c6802732eacefc53745b0522f199714f032
Yusril Septriandy. N. Y <yusrilseptriandy@gmail.com>
2024-06-18 11:22:19 +0700

    delete blank line

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -8,7 +8,6 @@ const Login = () => {
             w-full
             h-screen
             justify-center
-            
             bg-transparent
             p-5
             bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:5rem_4rem]"
