3262c8db6d7f59f600775eacd80d9bda91f56549
Yusril Septriandy. N. Y <yusrilseptriandy@gmail.com>
2024-06-18 12:40:55 +0700

    change background color - login page

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -12,8 +12,7 @@ const Login = () => {
             p-5
             bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:5rem_4rem]"
     >
-      <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[40%] translate-y-[-10%] rounded-full bg-[rgba(109,244,190,0.5)] opacity-50 blur-[80px] -z-10"></div>
-      <div className="absolute bottom-auto right-auto left-0 top-0 h-[500px] w-[500px] -translate-x-[10%] translate-y-40 rounded-full bg-[rgba(141,109,244,0.5)] opacity-50 blur-[80px] -z-10"></div>
+      <div className="absolute bottom-0 left-auto right-0 h-[500px] w-[500px] -translate-x-[0%] md:-translate-x-[60%] translate-y-[-50%] rounded-full bg-[rgba(109,125,244,0.8)] opacity-50 blur-[80px] -z-10"></div>
       <CardLogin />
     </div>
   );
