de2955750f4ddf2d78bdb40c25a2ae68b8757298
Yusril Septriandy. N. Y <yusrilseptriandy@gmail.com>
2024-06-18 12:51:30 +0700

    remove line backgeound

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -9,8 +9,7 @@ const Login = () => {
             h-screen
             justify-center
             bg-transparent
-            p-5
-            bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:5rem_4rem]"
+            p-5"
     >
       <div className="absolute bottom-0 left-auto right-0 h-[500px] w-[500px] -translate-x-[0%] md:-translate-x-[90%] translate-y-[-50%] rounded-full bg-[rgba(199,109,244,0.47)] opacity-50 blur-[80px] -z-10"></div>
       <CardLogin />
